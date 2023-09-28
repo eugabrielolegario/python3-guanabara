215a9185f65adc849c54d073ddcdd0c21b0762f0
eugabrielolegario <gabriel.oligar@gmail.com>
2023-09-28 13:51:22 -0300

    feat: :tada: Adiciona primeiros desafios do curso de Python

---
+++ Scripts python/soma-de-dois-numeros.py
@@ -0,0 +1,5 @@
+numero1 = input("Escolha um número")
+numero2 = input("Escolha outro número para fazer a soma")
+numero1 = float(numero1)
+numero2 = float(numero2)
+print("O resultado de", numero1, "+", numero2, "é", numero1 + numero2)
